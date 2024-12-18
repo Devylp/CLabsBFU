40e06645f12137441482db6ff735079d6c6cac86
Mr_Dev <jackvinston75@gmail.com>
2024-12-19 01:35:07 +0200

    Добавление 8.2 лр.

---
+++ CMakeProject1/LAB_8/LAB_8.h
@@ -0,0 +1,84 @@
+﻿#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector> 
+#include <sstream> // библиотека для работы с строками (их потоками)
+#pragma once
+
+
+// Разделим строку для дальнейшей обработки (разделяем в вектор)
+std::vector<std::string> SplitString(const std::string str) {
+    std::vector<std::string> result;
+    std::stringstream ss(str);
+    std::string item;
+
+    while (ss >> item) { // из ss мы записываем данные в item, а потом добавляем в вектор
+        result.push_back(item);
+    }
+
+    return result;
+}
+
+// Функция для перевода строки в нижний регистр для удобства
+std::string ToLower(std::string string1) {
+    for (char& c : string1) {
+        c = tolower(c);
+    }
+    return string1;
+}
+
+// Функция для подсчёта гласных
+int CountVowels(const std::string string3)
+{
+    int count = 0;
+
+    for (char c : string3) {
+        if (std::isalpha(c) && (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c == 'y'))
+        {
+            ++count;
+        }
+    }
+    return count;
+}
+
+bool WordContainConsonant(std::string word, size_t pos) {
+    if ((word[pos] >= 'a' && word[pos] <= 'z') && (word[pos] != 'a' && word[pos] != 'e' && word[pos] != 'i' && word[pos] != 'o' && word[pos] != 'u' && word[pos] != 'y')) {
+        return true;
+    }
+    else { return false; }
+}
+
+std::string ReverseSortWord(std::string word) {
+    std::sort(word.begin(), word.end(), std::greater<char>());
+    return word;
+}
+
+std::string CreateDublicateConsonant(std::string word) {
+    std::string result = "";
+    for (char c : word) {
+        if (std::isalpha(c)) {
+            if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c == 'y') {
+                result += c; // Гласные добавляем без изменений
+            }
+            else {
+                result += c;
+                result += c; // Согласные дублируем
+            }
+        }
+    }
+    return result;
+}
+
+std::vector<std::string> SortString(std::vector<std::string> string) {
+    std::vector<std::string> res;
+    for (int i = 0; i < string.size(); i++)
+    {
+        if (CountVowels(string[i]) >= 3) {
+            res.push_back(ReverseSortWord(string[i]));
+        }
+        else {
+            res.push_back(CreateDublicateConsonant(string[i]));
+        }
+    }
+    return res;
+}
---
+++ CMakeProject1/LAB_8/LAB_8_t2.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include "LAB_8.h"
+
+int main() {
+    
+    std::string line;
+    std::string result;
+
+    std::ifstream fin;
+    fin.open("C://Users//admin//source//repos//LAB_8//LAB_8//line.txt"); // Открываем файл 
+    
+    // Отработка ошибок
+    if (fin.is_open())
+    {
+        std::getline(fin, line);
+    }
+    fin.close();
+
+    SortString(SplitString(ToLower(line)));
+    
+    for (int i = 0; i < (SortString(SplitString(ToLower(line)))).size(); i++) {
+        std::cout << (SortString(SplitString(ToLower(line))))[i] << " ";
+    }
+}
